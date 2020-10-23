663954c01a32dedd1d690f2bec672df482316249
alessandro <alessandroadm@live.com>
2020-10-23 02:31:01 +0000

    feat: add createTask method

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,11 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { Task } from './task.model';
+import { TaskStatus } from './task.model';
+import {v1 as uuid} from 'uuid';
 
 @Injectable()
 export class TasksService {
- private tasks = [];
-
- getAllTasks() {
- return this.tasks
-}
-
+ private tasks:Task[] = [];
+ 
+ getAllTasks():Task[] {
+  return this.tasks
+ }
+ 
+ createTask(title:string, description:string): Task {
+  const task: Task = {
+   id: uuid(),
+   title,
+   description,
+   status:TaskStatus.OPEN
+  }
+  this.tasks.push(task);
+  return task;
+ }
+ 
 }
