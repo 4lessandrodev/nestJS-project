56cd8e259936a193707a338d93af7662fc5920ef
alessandro <alessandroadm@live.com>
2020-10-24 23:33:29 +0000

    feat: add docker compose setup

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -2,10 +2,10 @@ import { TypeOrmModuleOptions } from "@nestjs/typeorm";
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
  type: 'postgres',
- host: 'localhost',
+ host: 'postgres_db',
  port: 5432,
  username: 'postgres',
- password: 'admin',
+ password: 'postgres',
  database: 'taskmanagement',
  entities: [`${__dirname}/../**/*.entity.ts`],
  synchronize:true
